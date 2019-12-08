28756e93e459711a1c2d80c67f2738b2acba1e29
Shashi <sasiwebdev7@gmail.com>
2019-12-08 15:38:16 +0000

    MainSection1 and Mainsection2 merged
    MainSection1 and mainSection2 were merged and added a few media queries in the mobile version.

---
+++ js/javascript.js
@@ -0,0 +1,19 @@
+// let slideIndex = 0;
+
+// const showSlides = () => {
+//   let i;
+//   let slides = document.getElementsByClassName("sliders");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
+//   slideIndex++;
+
+//   if (slideIndex > slides.length) {
+//       slideIndex = 1
+//     }    
+//   slides[slideIndex-1].style.display = "block";  
+//   setTimeout(showSlides, 5000); // Change image every 3 seconds
+// }
+
+// showSlides();
+
